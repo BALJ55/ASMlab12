f9a0f92b9afd578a19a97b89181eba97cf5ba090
balj55 <byronamh@gmail.com>
2017-05-09 12:05:05 -0600

    commit inicial

---
+++ calculos.s
@@ -0,0 +1,27 @@
+
+ .text
+ .align 2
+ .global calculos
+ 
+ calculos:
+	VLRD S0, [r0]
+	VLRD S1, [r1]
+	VLRD S2, [r2]
+	VLRD S3, [r3]
+	VADD.f32 S0, S0,S1
+	VADD.f32 S2, S2,S3
+	VADD.f32 S0,S1  	@@suma de las cuatro notas
+	LDR R1, =cpc
+	VLDR S1, [R1]		@@en s1->0.4
+	VMUL.f32 S0,S0,S1	@@suma de las notas x 0.4
+	VSTR R0,S0			@@valor en r0
+	
+	MOV PC LR
+ 
+	
+	
+	
+	
+.data
+
+cpc: float 0.4
