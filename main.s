a17464491b3a077e47932885268cf2a2274a7da5
balj55 <byronamh@gmail.com>
2017-05-11 07:48:49 -0600

    compiler changes 3

--- main.s
+++ main.s
@@ -12,29 +12,33 @@
 	
 	@@parte de lectura de notas
 	
-	ldr r0,=mensaje
-	ldr r1,=num1
+	ldr r0,=num1
+	ldr r1,=num
 	bl scanf
-	mov r1,r0
+	ldr r0,=num1
+	ldr r0,[r0]
 	
 	
-	ldr r0,=mensaje
+	
+	ldr r0,=num2
+	ldr r1,=num
+	bl scanf
 	ldr r1,=num2
+	ldr r1,[r1]	
+	
+	
+	ldr r0,=num3
+	ldr r1,=num
 	bl scanf
-	mov r2,r0
+	ldr r2,=num2
+	ldr r2,[r2]
 	
 	
-	
-	ldr r0,=mensaje
-	ldr r1,=num3
+	ldr r0,=num4
+	ldr r1,=num
 	bl scanf
-	mov r3,r0
-	
-	
-	ldr r0,=mensaje
-	ldr r1,=num4
-	bl scanf
-	mov r4,r0
+	ldr r3,=num3
+	ldr r3,[r3]
 	
 	bl calculos
 	
@@ -58,5 +62,6 @@
 	 num2: .word "%f/n"
 	 num3: .word "%f/n"
 	 num4: .word "%f/n"
+	 num: 	.word 0
  
 	 notaProyecto: .float 0.61
